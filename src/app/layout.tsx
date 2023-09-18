9c3adb433e232bd2dd4094c70d75d6bd260a0308
wolfgunblood <jalajdorai1@gmail.com>
2023-09-18 18:16:06 +0530

    Added Card header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className='min-h-screen'>
           {children}
+          </div>
 
           <ThemeToggle className=" absolute top-6 right-6"  />
         </ThemeProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-red-600'>hello world</h1>
-  )
+    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      <Card>
+        <CardHeader>
+        <CardTitle>
+          Register
+        </CardTitle>
+        <CardDescription>
+          Register to get started
+        </CardDescription>
+        </CardHeader>
+        <CardContent>
+
+        </CardContent>
+      </Card>
+    </div>)
 }
